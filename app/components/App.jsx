78758a952747fcd05c735a273d5964889e7e1883
Michael Allen <mike@mistColoured.com>
2018-06-12 16:38:55 +0100

    Initial commit

---
+++ app/components/App.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+export default class App extends Component {
+    render(){
+        return (
+            <div>
+                >>> 
+                booom !!!zztop ?? !! .... Damn Oh yesThe really real Mike says Hello From React v15.4.3
+            </div>
+        );
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+
+module.exports = {
+    entry: './app/main.js',
+    output: {
+        filename: 'bundle.js'
+    },
+    mode: 'development',
+    module: {
+        // configuration regarding modules
+        rules: [
+            // rules for modules (configure loaders, parser options, etc.)
+            {
+                test: /\.jsx?$/,
+                exclude: [
+                    path.resolve(__dirname, "node_modules")
+                ],
+                // these are matching conditions, each accepting a regular expression or string
+                // test and include have the same behavior, both must be matched
+                // exclude must not be matched (takes preferrence over test and include)
+                // Best practices:
+                // - Use RegExp only in test and for filename matching
+                // - Use arrays of absolute paths in include and exclude
+                // - Try to avoid exclude and prefer include
+                loader: 'babel-loader',
+                // the loader which should be applied, it'll be resolved relative to the context
+                // -loader suffix is no longer optional in webpack2 for clarity reasons
+                // see webpack 1 upgrade guide
+                options: {
+                    presets: ["es2015"]
+                },
+                // options for the loader
+            }
+        ]
+    },
+    // lets you precisely control what bundle information gets displayed
+    devServer: {
+        compress: true,
+        hot: true,
+        stats: "minimal",
+        port: 8080
+    }
+}
