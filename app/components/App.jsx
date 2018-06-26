5fc9580ffbf3775c3c6a653ce25696b1dbbb9e28
Michael Allen <mike@mistColoured.com>
2018-06-27 00:02:50 +0100

    Converted App.jsx into a stateless component

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
-export default class App extends Component {
-    render() {
-        return (
-            <div>
-                >>>
-                fgdfgbooom !!!zztop ?? !! .... Damn Oh yesThe extra really real Mike says Hello From React v15.4.3
-            </div>
-        );
-    }
-}
+import React from 'react';
+
+const App = () => (
+  <div>
+    Mike says Hello From React v15.4.3
+  </div>
+);
+
+export default App;
