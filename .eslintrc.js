0945a3a17bc4c8c736bc33631123038e87d65008
Michael Allen <mike@mistColoured.com>
2018-06-29 10:00:28 +0100

    Added some custom eslint rules
    Less friction while practicing

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,17 @@
 module.exports = {
-    "env": {
-      "browser": true,
-      "es6": true,
-      "node": true
-    },
-    "extends": [
-      "airbnb"
-    ],
-    "parser": "babel-eslint",
-    "rules": {
-      // Add own custom rules here as needed
-    }
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true
+  },
+  'extends': [
+    'airbnb'
+  ],
+  'parser': 'babel-eslint',
+  'rules': {
+    // Disabled just while learning
+    'react/jsx-one-expression-per-line': 'off',
+    'react/button-has-type': 'off',
+    // Permanent preferences
   }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,72 @@
-const path = require('path');
+const PATH = require('path');
+
 const PORT = process.env.PORT || 8080;
 
 module.exports = {
-    entry: './app/main.js',
-    output: {
-        filename: 'bundle.js'
-    },
-    mode: 'development',
-    module: {
-        // configuration regarding modules
-        rules: [
-            // rules for modules (configure loaders, parser options, etc.)
-            {
-                test: /\.jsx?$/,
-                exclude: [
-                    path.resolve(__dirname, "node_modules")
-                ],
-                // these are matching conditions, each accepting a regular expression or string
-                // test and include have the same behavior, both must be matched
-                // exclude must not be matched (takes preferrence over test and include)
-                // Best practices:
-                // - Use RegExp only in test and for filename matching
-                // - Use arrays of absolute paths in include and exclude
-                // - Try to avoid exclude and prefer include
-                loader: 'babel-loader',
-                // the loader which should be applied, it'll be resolved relative to the context
-                // -loader suffix is no longer optional in webpack2 for clarity reasons
-                // see webpack 1 upgrade guide
-                options: {
-                    presets: [
-                        "env",
-                        "react",
-                        "stage-3"
-                    ]
-                },
-                // options for the loader
-            },
-            {
-                // Disabled until I need to add CSS styling to my work again
-                // test: /\.scss$/,
-                // use: [
-                //   'style-loader',
-                //   'css-loader',
-                //   'sass-loader'
-                // ] 
-            }
-        ]
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        compress: true,
-        hot: true,
-        stats: "minimal", 
-        // lets you precisely control what bundle information gets displayed
-        port: PORT
-    }
-}
+  entry: './app/main.js',
+  output: {
+    filename: 'bundle.js',
+  },
+  mode: 'development',
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.json'],
+    // alias: {
+    //     MyFiles: PATH.resolve(__dirname, 'src/')
+    // }
+  },
+  module: {
+    // configuration regarding modules
+    rules: [
+      // rules for modules (configure loaders, parser options, etc.)
+      {
+        // these are matching conditions, each accepting a regular expression or string
+        // test and include have the same behavior, both must be matched
+        // exclude must not be matched (takes preferrence over test and include)
+        // Best practices:
+        // - Use RegExp only in test and for filename matching
+        // - Use arrays of absolute paths in include and exclude
+        // - Try to avoid exclude and prefer include
+        test: /\.jsx?$/,
+        exclude: [
+          PATH.resolve(__dirname, 'node_modules'),
+        ],
+        // the loader which should be applied, it'll be resolved relative to the context
+        // -loader suffix is no longer optional in webpack2 for clarity reasons
+        // see webpack 1 upgrade guide
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            'env',
+            'react',
+            'stage-3',
+          ],
+        },
+        // options for the loader
+      },
+      {
+        // Disabled until I need to add CSS styling to my work again
+        // test: /\.scss$/,
+        // use: [
+        //   'style-loader',
+        //   'css-loader',
+        //   'sass-loader'
+        // ]
+      },
+    ],
+  },
+  devServer: {
+    // Tell the server where to serve content from.
+    // This is only necessary if you want to serve static files.
+    contentBase: PATH.join(__dirname, 'public'),
+    // Enable gzip compression for everything served
+    compress: true,
+    // Enable webpack's Hot Module Replacement feature
+    hot: true,
+    // This option lets you precisely control what bundle information gets
+    // displayed. This can be a nice middle ground if you want some bundle
+    // information, but not all of it.
+    stats: 'minimal',
+    port: PORT,
+    open: true,
+  },
+};
