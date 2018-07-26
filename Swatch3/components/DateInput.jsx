09e9a04626e2472b2033ee6a64588871c2eaddd0
Michael Allen <mike@mistColoured.com>
2018-07-26 22:08:36 +0100

    Renamed Input component to DateInput.
    More descriptive.

--- Swatch3/components/DateInput.jsx
+++ Swatch3/components/DateInput.jsx
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
-// import moment from 'moment';
-import Input from './Input';
+import moment from 'moment';
 
-export default class DateUntil extends Component {
+export default class Input extends Component {
+  state = {
+    inputDate: '',
+    validDate: true,
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Submit');
+  }
+
+  handleChange = (e) => {
+    console.log('Change');
+    this.setState({
+      inputDate: e.target.value,
+      validDate: moment(e.target.value, 'DD MMM YYYY').isValid(),
+    });
+  }
+
   render() {
-    const { dateUntil } = this.props;
     return (
       <div>
-        <Input />
+        <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <input
+              onChange={this.handleChange}
+              type="text"
+              className="form-control"
+              id="exampleInputEmail1"
+              placeholder="Enter email"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">{this.state.validDate ? 'True' : 'False'}</button>
+        </form>
       </div>
     );
   }
