3ce6d7b00d8e395fdd566efcce38c1a9811406ec
Michael Allen <mike@mistColoured.com>
2018-07-26 12:58:57 +0100

    Initial commit. Time offset display module

---
+++ Swatch3/components/Delta.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import moment from 'moment';
+import PropTypes from 'prop-types';
+
+import TimeUnit from './TimeUnit';
+
+const Delta = ({ time }) => (
+  <div className="card-deck text-center">
+    <TimeUnit number={moment.duration(time).asDays()} unit="day" />
+    <TimeUnit number={moment.duration(time).hours()} unit="hour" />
+    <TimeUnit number={moment.duration(time).minutes()} unit="min" />
+    <TimeUnit number={moment.duration(time).seconds()} unit="sec" />
+  </div>
+);
+
+Delta.propTypes = {
+  time: PropTypes.number,
+};
+
+Delta.defaultProps = {
+  time: 0,
+};
+export default Delta;
