cc3c4f6c147f1f6f7df71566391eb002b9498d95
Michael Allen <mike@mistColoured.com>
2018-07-26 18:13:56 +0100

    Initial commit. First attempt at time difference component.

---
+++ Swatch3/components/DateUntil.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import moment from 'moment';
+import Input from './Input';
+
+export default class DateUntil extends Component {
+  render() {
+    const { dateUntil } = this.props;
+    return (
+      <div>
+        <Input />
+
+
+      </div>
+    );
+  }
+}
