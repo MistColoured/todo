575af72013f5f86823083da6ceda179a025c5abd
Michael Allen <mike@mistColoured.com>
2018-07-29 11:42:14 +0100

    Added dummy eventDates array for mapping multiple elements code.

--- Swatch3/index.jsx
+++ Swatch3/index.jsx
@@ -1,37 +1,36 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 import Time from './components/Time';
-import Delta from './components/Delta';
+import OffsetList from './components/OffsetList';
 import ToggleDateInput from './components/ToggleDateInput';
 
 class App extends Component {
   state = {
     currentTime: moment(),
-    dateUntil: moment('17 Aug 2018'),
-    delta: this.getDelta,
+    eventDates: [
+      moment('17 Aug 2018'),
+      moment('12 Aug 2018'),
+      moment('25 Dec 2018'),
+    ],
+    // delta: moment('17 August 2018').diff(moment()),
   }
 
   componentDidMount = () => {
     this.timerId = setInterval(() => {
       this.setState({
         currentTime: moment(),
-        delta: this.getDelta(),
+        // delta: this.getDelta(),
       });
     }, 1000);
   }
 
-  getDelta = () => {
-    const { currentTime, dateUntil } = this.state;
-    return dateUntil.diff(currentTime);
-  }
-
   render() {
-    const { delta, currentTime } = this.state;
+    const { eventDates, currentTime } = this.state;
     return (
       <div className="container text-center">
         <Time time={currentTime} message="Current time" />
-        <Delta time={delta} />
-        <ToggleDateInput />
+        <OffsetList offsets={eventDates} current={currentTime} />
+        {/* <ToggleDateInput /> */}
       </div>
     );
   }
