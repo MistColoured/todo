5686213ed1bb1ce1d00ddb40475a75acd2e0c8f2
Michael Allen <mike@mistColoured.com>
2018-07-31 20:43:29 +0100

    Added external <div> to contain title of date.

--- Swatch3/components/Time.jsx
+++ Swatch3/components/Time.jsx
@@ -5,10 +5,13 @@ import PropTypes from 'prop-types';
 import TimeUnit from './TimeUnit';
 
 const Time = ({ time }) => (
-  <div className="card-deck mb-3 text-center">
-    <TimeUnit number={time.hours()} />
-    <TimeUnit number={time.minutes()} />
-    <TimeUnit number={time.seconds()} />
+  <div className="container text-center">
+    <p>The current time is:</p>
+    <div className="card-deck mb-3 text-center">
+      <TimeUnit number={time.hours()} />
+      <TimeUnit number={time.minutes()} />
+      <TimeUnit number={time.seconds()} />
+    </div>
   </div>
 );
 
