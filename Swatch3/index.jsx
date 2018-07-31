7e1d2956272a2804a11578f0a3dfc9bcaf7669a5
Michael Allen <mike@mistColoured.com>
2018-07-31 19:00:56 +0100

    Change to state management for child SearchBar component.

--- Swatch3/index.jsx
+++ Swatch3/index.jsx
@@ -1,40 +1,61 @@
 import React, { Component } from 'react';
 import moment from 'moment';
+import axios from 'axios';
 import Time from './components/Time';
 import OffsetList from './components/OffsetList';
-import ToggleDatePicker from './components/ToggleDatePicker';
+import SearchBar from './components/SearchBar';
+// import ToggleDatePicker from './components/ToggleDatePicker';
 
 class App extends Component {
+  myUrl = 'https://5b5f35c58e9f160014b88dce.mockapi.io/api/eventDates'
+
   state = {
     currentTime: moment(),
-    eventDates: [
-      moment('17 Aug 2018'),
-      moment('12 Aug 2018'),
-      moment('25 Dec 2018'),
-    ],
+    eventDates: [],
+    inputDate: '',
     // delta: moment('17 August 2018').diff(moment()),
   }
 
   componentDidMount = () => {
+    axios.get(this.myUrl)
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          eventDates: res.data,
+        });
+      });
     this.timerId = setInterval(() => {
       this.setState({
         currentTime: moment(),
-        // delta: this.getDelta(),
       });
     }, 1000);
   }
 
-  toggleDatePicker = () => {
-    this.setState(prevState => ({
-      showDatePicker: !prevState.showDatePicker,
-    }));
+  handleInputChange = (inputDate) => {
+    console.log('Change');
+    this.setState({
+      inputDate,
+    });
+  }
+
+  handleFormSubmit = () => {
+    const { inputDate } = this.state;
+    console.log('Form submitted', inputDate);
+    // this.setState({
+    //   inputDate: '',
+    // });
   }
 
   render() {
-    const { eventDates, currentTime } = this.state;
+    const { eventDates, currentTime, inputDate } = this.state;
     return (
       <div className="container text-center">
-        <ToggleDatePicker />
+        {/* <ToggleDatePicker /> */}
+        <SearchBar
+          onInputChange={this.handleInputChange}
+          onFormSubmit={this.handleFormSubmit}
+          inputDate={inputDate}
+        />
         <Time time={currentTime} message="Current time" />
         <OffsetList offsets={eventDates} current={currentTime} />
       </div>
