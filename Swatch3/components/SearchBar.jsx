a7dc52c4890453893343fbeb6ebe699472c7795c
Michael Allen <mike@mistColoured.com>
2018-08-01 11:44:16 +0100

    + fontawesome

--- Swatch3/components/SearchBar.jsx
+++ Swatch3/components/SearchBar.jsx
@@ -2,6 +2,11 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import moment from 'moment';
+// import icons from 'glyphicons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
+// const element = <FontAwesomeIcon icon={faCoffee} />;
 
 export default class SearchBar extends Component {
   handleInputChange = (e) => {
@@ -20,26 +25,45 @@ export default class SearchBar extends Component {
     return (
       <div>
         <form
+          className="form-inline"
           onSubmit={this.handleFormSubmit}
           autoComplete="off"
         >
-          <div className="form-group mt-4">
+
+
+          <div className="form-group col-md-4">
+            <label htmlFor="eventName" className="sr-only">Event name</label>
             <input
               value={inputDate}
               onChange={this.handleInputChange}
               type="text"
               className="form-control"
-              id="dateInput"
-              placeholder="Enter date"
+              id="eventName"
+              placeholder="Name of event"
             />
+          </div>
+          <div className="form-group col-md-4">
+            <label htmlFor="dateInput" className="sr-only">Date Input</label>
+            <div className="input-group">
+              <input
+                value={inputDate}
+                onChange={this.handleInputChange}
+                type="text"
+                className="form-control"
+                id="dateInput"
+                placeholder="Enter date"
+              />
+
+            </div>
           </div>
           <button
             disabled={!moment(inputDate).isValid()}
             type="submit"
-            className="btn btn-primary mb-4"
+            className="btn btn-primary"
           >
-            Submit
+        Submit
           </button>
+
         </form>
       </div>
     );
