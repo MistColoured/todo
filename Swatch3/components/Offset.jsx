1481be92c64fd9ec06af37662db89511d2965911
Michael Allen <mike@mistColoured.com>
2018-08-02 21:23:33 +0100

    Added title info to events.

--- Swatch3/components/Offset.jsx
+++ Swatch3/components/Offset.jsx
@@ -4,12 +4,19 @@ import PropTypes from 'prop-types';
 
 import TimeUnit from './TimeUnit';
 
-const Offset = ({ time }) => (
-  <div className="card-deck mb-3 text-center">
-    <TimeUnit number={moment.duration(time).asDays()} unit="day" />
-    <TimeUnit number={moment.duration(time).hours()} unit="hour" />
-    <TimeUnit number={moment.duration(time).minutes()} unit="min" />
-    <TimeUnit number={moment.duration(time).seconds()} unit="sec" />
+const Offset = ({ time, element: { eventName, eventDate, id }, onDelete }) => (
+  <div className="container text-center">
+    <p>
+      {eventName} (
+      {moment(eventDate).format('ddd, D MMMM')})
+      <button onClick={onDelete('4')} className="btn btn-danger">Delete</button>
+    </p>
+    <div className="card-deck mb-3 text-center">
+      <TimeUnit number={moment.duration(time).asDays()} unit="day" />
+      <TimeUnit number={moment.duration(time).hours()} unit="hour" />
+      <TimeUnit number={moment.duration(time).minutes()} unit="min" />
+      <TimeUnit number={moment.duration(time).seconds()} unit="sec" />
+    </div>
   </div>
 );
 
