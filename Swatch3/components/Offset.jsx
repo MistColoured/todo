90d6de535c2b0113f3562bf75f220e2f1456bd08
Michael Allen <mike@mistColoured.com>
2018-08-03 10:50:22 +0100

    Added nested PropTypes.

--- Swatch3/components/Offset.jsx
+++ Swatch3/components/Offset.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
+// eslint-disable-next-line
 import moment from 'moment';
 import PropTypes from 'prop-types';
 
 import TimeUnit from './TimeUnit';
 
-// TODO Add a destructured PropType for the elem object.
 const Offset = ({ time, item: { eventName, eventDate, id }, onDelete }) => (
   <div className="container text-center">
     <p>
       {eventName} (
       {moment(eventDate).format('ddd, D MMMM')})
-      <button id={id} onClick={() => onDelete(id = { id })} className="btn btn-danger">Delete</button>
+      <button id={id} onClick={() => onDelete(id)} className="btn btn-danger">Delete</button>
     </p>
     <div className="card-deck mb-3 text-center">
       <TimeUnit number={moment.duration(time).asDays()} unit="day" />
@@ -23,9 +23,21 @@ const Offset = ({ time, item: { eventName, eventDate, id }, onDelete }) => (
 
 Offset.propTypes = {
   time: PropTypes.number,
+  item: PropTypes.shape({
+    eventName: PropTypes.string,
+    eventDate: PropTypes.string,
+    id: PropTypes.number,
+  }),
+  onDelete: PropTypes.func,
 };
 
 Offset.defaultProps = {
   time: 0,
+  item: PropTypes.shape({
+    eventName: '',
+    eventDate: '',
+    id: 0,
+  }),
+  onDelete() {},
 };
 export default Offset;
