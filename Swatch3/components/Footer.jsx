42c8a9faca203faffd18d78a48ffdbc1a1834384
Michael Allen <mike@mistColoured.com>
2018-08-07 22:42:23 +0100

    Added contact icons with hrefs.

--- Swatch3/components/Footer.jsx
+++ Swatch3/components/Footer.jsx
@@ -1,58 +1,36 @@
-/* eslint-disable */
-
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const Footer = () => (
-  // <div>
-  //   <footer className="footer fixed-bottom">
-  //     <div className="container">
-  //       <span className="text-muted">
+  <Fragment>
+    <i className="flex-item">Contact Me</i>
+    <a href="https://www.facebook.com/MistColoured-370431490157581/"><i className="flex-item fab fa-facebook-square fa-3x" /></a>
 
-  //         <a href="https://www.facebook.com/MistColoured-370431490157581/" className="icons-sm fb-ic mr-3"><i className="fab fa-facebook-square fa-2x" /></a>
+    <a href="https://twitter.com/mistColoured?lang=en-gb"><i className="flex-item fab fa-twitter-square fa-3x" /></a>
 
-  //         <a href="https://twitter.com/mistColoured?lang=en-gb" className="icons-sm tw-ic mr-3"><i className="fab fa-twitter-square fa-2x" /></a>
+    <a href="https://plus.google.com/u/0/102413310267150819391"><i className="flex-item fab fa-google-plus-square fa-3x" /></a>
 
-  //         <a href="" className="icons-sm gplus-ic mr-3"><i className="fab fa-google-plus-square fa-2x" /></a>
+    <a href="https://www.linkedin.com/in/michael-allen-545a0914b/"><i className="flex-item fab fa-linkedin fa-3x" /></a>
 
-  //         <a href="https://www.linkedin.com/in/michael-allen-545a0914b/" className="icons-sm li-ic mr-3"><i className="fab fa-linkedin fa-2x" /></a>
+    <a href="https://www.instagram.com/mistcoloured/?hl=undefined"><i className="flex-item fab fa-instagram fa-3x" /></a>
 
-  //         <a href="" className="icons-sm ins-ic mr-3"><i className="fab  fa-instagram fa-2x" /></a>
+    <a href="https://www.pinterest.co.uk/mistcoloured"><i className="flex-item fab  fa-pinterest fa-3x" /></a>
 
-  //         <a href="https://www.pinterest.co.uk/mistcoloured" className="icons-sm pin-ic mr-3"><i className="fab  fa-pinterest fa-2x" /></a>
+    {/* <a href="" className="flex-item"><i className="fab fa-youtube " /></a> */}
 
-  //         {/* <a href="" className="icons-sm yt-ic mr-3"><i className="fab fa-youtube fa-2x" /></a> */}
+    {/* <a href="" className="flex-item"><i className="fab fa-dribbble " /></a> */}
 
-  //         {/* <a href="" className="icons-sm dribble-ic mr-3"><i className="fab fa-dribbble fa-2x" /></a> */}
+    {/* <a><i className="flex-item fab fa-stack-overflow fa-3x" /></a> */}
 
-  //         <a className="icons-sm so-ic mr-3"><i className="fab fa-stack-overflow fa-2x" /></a>
+    {/* <a><i className="flex-item fab fa-slack fa-3x" /></a> */}
 
-  //         <a className="icons-sm slack-ic mr-3"><i className="fab fa-slack fa-2x" /></a>
+    <a href="https://github.com/MistColoured"><i className="flex-item fab fa-github fa-3x" /></a>
 
-  //         <a className="icons-sm git-ic mr-3"><i className="fab fa-github fa-2x" /></a>
+    {/* <a><i className="flex-item fas fa-comments fa-3x" /></a> */}
 
-  //         <a className="icons-sm comm-ic mr-3"><i className="fas fa-comments fa-2x" /></a>
+    {/* <a className="flex-item"><i className="fas fa-envelope-square " /></a> */}
 
-  //         <a className="icons-sm email-ic mr-3"><i className="fas fa-envelope-square fa-2x" /></a>
-
-  //         <a className="icons-sm reddit-ic"><i className="fab fa-reddit fa-2x" /></a>
-
-
-  //       </span>
-  //     </div>
-  //   </footer>
-  // </div>
-  <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
-    <h5 className="my-0 mr-md-auto font-weight-normal">mistColoured</h5>
-    <h5 className="my-0 mr-md-auto font-weight-normal">my very first app</h5>
-
-    <nav className="my-2 my-md-0 mr-md-3">
-      <a className="p-2 text-dark" href="#">About Me</a>
-      <a className="p-2 text-dark" href="#">My Apps</a>
-      <a className="p-2 text-dark" href="#">C.V.</a>
-      <a className="p-2 text-dark" href="#">Contact Me</a>
-    </nav>
-    <a className="btn btn-outline-primary" href="#">Sign up</a>
-  </div>
+    <a href="https://www.reddit.com/user/MistColoured/"><i className="flex-item fab fa-reddit fa-3x" /></a>
+  </Fragment>
 );
 
 export default Footer;
