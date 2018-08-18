11dddd7f81970a499e7f29a1a1626d816fa588a6
Michael Allen <mike@mistColoured.com>
2018-08-18 17:49:10 +0100

    Removed WebFont

--- index.jsx
+++ index.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import WebFont from 'webfontloader';
+// import WebFont from 'webfontloader';
 import App from './Swatch3';
 
 // import 'semantic-ui-css/semantic.min.css';
 import './Swatch3/styles/style.scss';
 
-WebFont.load({
-  google: {
-    families: ['Poiret One:300,400,700', 'cursive'],
-  },
-});
+// WebFont.load({
+//   google: {
+//     families: ['Poiret One:300,400,700', 'cursive'],
+//   },
+// });
 
 ReactDOM.render(<App />, document.getElementById('app-root'));
