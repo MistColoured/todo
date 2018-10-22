ff44a3516a294646a079ca680f07ebe59a4963bb
Michael Allen <mike@mistColoured.com>
2018-10-22 19:39:38 +0100

    Changed config.
    Deleted old DB after trying Firestone DB for a while and reverting back to Realtime.

--- ToDo/components/firebase.js
+++ ToDo/components/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/database';
 import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDqMHO_WqN4tskrPqKeyu0E2l3uJWJPAuQ",
-  authDomain: "todo-ecd7b.firebaseapp.com",
-  databaseURL: "https://todo-ecd7b.firebaseio.com",
-  projectId: "todo-ecd7b",
-  storageBucket: "todo-ecd7b.appspot.com",
-  messagingSenderId: "419600733994"
+  apiKey: 'AIzaSyDeKQ6etjKP5oh2y8CbQaKcdHjK9HhnhbI',
+  authDomain: 'todo-5ec95.firebaseapp.com',
+  databaseURL: 'https://todo-5ec95.firebaseio.com',
+  projectId: 'todo-5ec95',
+  storageBucket: '',
+  messagingSenderId: '514275945318',
 };
 firebase.initializeApp(config);
 
